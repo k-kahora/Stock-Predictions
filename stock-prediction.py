5e5c37b8e5a479576c308d0b4f97752b2581c59a
Malcolm Kahora <mango22mjk@gmail.com>
2023-03-26 19:24:38 -0400

    Testing

--- stock-prediction.py
+++ stock-prediction.py
@@ -6,14 +6,94 @@ from finta import TA
 import matplotlib.pyplot as plt
 
 from sklearn import svm
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier, VotingClassifier 
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.ensemble import AdaBoostClassifier
-from sklearn.ensemble import GradientBoostingClassifier
-from sklearn.ensemble import VotingClassifier
 from sklearn.model_selection import train_test_split, GridSearchCV
-from sklearn.metrics import confusion_matrix, classification_report
-from sklearn import metrics
+from sklearn.metrics import confusion_matrix, classification_report, mean_squared_error, accuracy_score
+
+def _get_indicator_data(data):
+    """
+    Function that uses the finta API to calculate technical indicators used as the features
+    :return:
+    """
+
+    for indicator in INDICATORS:
+        ind_data = eval('TA.' + indicator + '(data)')
+        if not isinstance(ind_data, pd.DataFrame):
+            ind_data = ind_data.to_frame()
+        data = data.merge(ind_data, left_index=True, right_index=True)
+    data.rename(columns={"14 period EMV.": '14 period EMV'}, inplace=True)
+
+    # Also calculate moving averages for features
+    data['ema50'] = data['close'] / data['close'].ewm(50).mean()
+    data['ema21'] = data['close'] / data['close'].ewm(21).mean()
+    data['ema15'] = data['close'] / data['close'].ewm(14).mean()
+    data['ema5'] = data['close'] / data['close'].ewm(5).mean()
+
+    # Instead of using the actual volume value (which changes over time), we normalize it with a moving volume average
+    data['normVol'] = data['volume'] / data['volume'].ewm(5).mean()
+
+    # Remove columns that won't be used as features
+    del (data['open'])
+    del (data['high'])
+    del (data['low'])
+    del (data['volume'])
+    del (data['Adj Close'])
+    
+    return data
+
+def _exponential_smooth(data, alpha):
+    """
+    Function that exponentially smooths dataset so values are less 'rigid'
+    :param alpha: weight factor to weight recent values more
+    """
+    
+    return data.ewm(alpha=alpha).mean()
+
+def _produce_prediction(data, window):
+    """
+    Function that produces the 'truth' values
+    At a given row, it looks 'window' rows ahead to see if the price increased (1) or decreased (0)
+    :param window: number of days, or rows to look ahead to see what the price did
+    """
+    
+    prediction = (data.shift(-window)['close'] >= data['close'])
+    prediction = prediction.iloc[:-window]
+    data['pred'] = prediction.astype(int)
+    
+    return data
+
+def _train_random_forest(X_train, y_train, X_test, y_test):
+
+    """
+    Function that uses random forest classifier to train the model
+    :return:
+    """
+    
+    # Create a new random forest classifier
+    rf = RandomForestClassifier()
+    
+    # Dictionary of all values we want to test for n_estimators
+    params_rf = {'n_estimators': [110,130,140,150,160,180,200]}
+    
+    # Use gridsearch to test all values for n_estimators
+    rf_gs = GridSearchCV(rf, params_rf, cv=5)
+    
+    # Fit model to training data
+    rf_gs.fit(X_train, y_train)
+    
+    # Save best model
+    rf_best = rf_gs.best_estimator_
+    
+    # Check best n_estimators value
+    print(rf_gs.best_params_)
+    
+    prediction = rf_best.predict(X_test)
+
+    print(classification_report(y_test, prediction))
+    print(confusion_matrix(y_test, prediction))
+    
+    return rf_best
 
 """
 Defining some constants for data mining
@@ -26,10 +106,8 @@ symbol = 'SPY'      # Symbol of the desired stock
 # List of symbols for technical indicators
 INDICATORS = ['RSI', 'MACD', 'STOCH','ADL', 'ATR', 'MOM', 'MFI', 'ROC', 'OBV', 'CCI', 'EMV', 'VORTEX']
 
-"""
-Next we pull the historical data using yfinance
-Rename the column names because finta uses the lowercase names
-"""
+
+
 """
 Next we pull the historical data using yfinance
 Rename the column names because finta uses the lowercase names
@@ -41,21 +119,27 @@ end = datetime.datetime.today()
 data = yf.download(symbol, start=start, end=end, interval=INTERVAL)
 data.rename(columns={"Close": 'close', "High": 'high', "Low": 'low', 'Volume': 'volume', 'Open': 'open'}, inplace=True)
 
-print(data.head())
+print(data.to_json(orient='records'))
+print(data)
 
+data = _exponential_smooth(data, 0.3)
+data = _get_indicator_data(data)
+data = _produce_prediction(data, window=15)
 
-def _exponential_smooth(data, alpha):
+del (data['close'])
+data = data.dropna() # Some indicators produce NaN values for the first few rows, we just remove them here
+data.tail()
 
-    """
-    Function that exponentially smooths dataset so values are less 'rigid'
-    :param alpha: weight factor to weight recent values more
-    """
-    
-    return data.ewm(alpha=alpha).mean()
-
-data = _exponential_smooth(data, 0.65)
+print(data.columns)
+live_pred_data = data.iloc[-16:-11]
 
 tmp = data.iloc[-60:]
+
+
+
 tmp['close'].plot()
+
+
+
 
 plt.show()

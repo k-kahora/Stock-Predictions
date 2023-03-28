8178d6b6a88ea5e665eb3b4c8bb7df370d0e573b
Malcolm Kahora <mango22mjk@gmail.com>
2023-03-28 17:06:25 -0400

    First commit with new attempt

--- stock-prediction.py
+++ stock-prediction.py
@@ -1,61 +1,14 @@
-import yfinance as yf
-import datetime
 import pandas as pd
-import numpy as np
-from finta import TA
-import matplotlib.pyplot as plt
+stock_data = pd.read_csv("./NFLX.csv", index_col="Date")
 
-from sklearn import svm
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.ensemble import AdaBoostClassifier
-from sklearn.ensemble import GradientBoostingClassifier
-from sklearn.ensemble import VotingClassifier
-from sklearn.model_selection import train_test_split, GridSearchCV
-from sklearn.metrics import confusion_matrix, classification_report
-from sklearn import metrics
+import matplotlib.dates as mdates
+import matplotlib.dates as plt
+import datetime as dt
 
-"""
-Defining some constants for data mining
-"""
-
-NUM_DAYS = 10000     # The number of days of historical data to retrieve
-INTERVAL = '1d'     # Sample rate of historical data
-symbol = 'SPY'      # Symbol of the desired stock
-
-# List of symbols for technical indicators
-INDICATORS = ['RSI', 'MACD', 'STOCH','ADL', 'ATR', 'MOM', 'MFI', 'ROC', 'OBV', 'CCI', 'EMV', 'VORTEX']
-
-"""
-Next we pull the historical data using yfinance
-Rename the column names because finta uses the lowercase names
-"""
-"""
-Next we pull the historical data using yfinance
-Rename the column names because finta uses the lowercase names
-"""
-
-start = (datetime.date.today() - datetime.timedelta( NUM_DAYS ) )
-end = datetime.datetime.today()
-
-data = yf.download(symbol, start=start, end=end, interval=INTERVAL)
-data.rename(columns={"Close": 'close', "High": 'high', "Low": 'low', 'Volume': 'volume', 'Open': 'open'}, inplace=True)
-
-print(data.head())
+plt.figure(figsize=(15,10))
+plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))
+plt.gca().xaxis.set_major_locator(mdates.DayLocator(interval=60))
+x_dates = [dt.datetime.strptime(d, '%Y-%m-%m').date() for d in stock_data.index.values]
 
 
-def _exponential_smooth(data, alpha):
 
-    """
-    Function that exponentially smooths dataset so values are less 'rigid'
-    :param alpha: weight factor to weight recent values more
-    """
-    
-    return data.ewm(alpha=alpha).mean()
-
-data = _exponential_smooth(data, 0.65)
-
-tmp = data.iloc[-60:]
-tmp['close'].plot()
-
-plt.show()

b79c91fa77898a9810ad2b799dbb311ddd77e6c9
Malcolm Kahora <mango22mjk@gmail.com>
2023-03-28 17:21:13 -0400

    Hight and low graph

--- stock-prediction.py
+++ stock-prediction.py
@@ -2,13 +2,21 @@ import pandas as pd
 stock_data = pd.read_csv("./NFLX.csv", index_col="Date")
 
 import matplotlib.dates as mdates
-import matplotlib.dates as plt
+import matplotlib.pyplot as plt
 import datetime as dt
 
 plt.figure(figsize=(15,10))
 plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))
 plt.gca().xaxis.set_major_locator(mdates.DayLocator(interval=60))
-x_dates = [dt.datetime.strptime(d, '%Y-%m-%m').date() for d in stock_data.index.values]
+x_dates = [dt.datetime.strptime(d, '%Y-%m-%d').date() for d in stock_data.index.values]
+
+plt.plot(x_dates, stock_data["High"], label="High")
+plt.plot(x_dates, stock_data["Low"], label="Low")
+plt.xlabel('Time Scale')
+plt.xlabel('Scaled USD')
+plt.legend()
+plt.gcf().autofmt_xdate()
+plt.show()
 
 
 

b0dd51edab8370be97d5e3e42dcdc3c54ef39484
Malcolm Kahora <mango22mjk@gmail.com>
2023-03-28 17:38:18 -0400

    lstm funcion

--- stock-prediction.py
+++ stock-prediction.py
@@ -1,9 +1,42 @@
 import pandas as pd
-stock_data = pd.read_csv("./NFLX.csv", index_col="Date")
-
 import matplotlib.dates as mdates
 import matplotlib.pyplot as plt
 import datetime as dt
+import numpy as np
+
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import LSTM
+from tensorflow.keras.layers import Dropout
+from tensorflow.keras.layers import *
+from tensorflow.keras.callbacks import EarlyStopping
+
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_absolute_percentage_error
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import TimeSeriesSplit
+
+def lstm_split(data, n_steps):
+    X, y = [], []
+    for i in range(len(data)-n_steps+1):
+        X.append(data[i:i + n_steps, :-1])
+        y.append(data[i + n_steps-1, -1])
+
+    return np.array(X), np.array(y)
+
+stock_data = pd.read_csv("./NFLX.csv", index_col="Date")
+
+target_y = stock_data['Close']
+X_feat = stock_data.iloc[:,0:3]
+
+# Feature Scaling
+sc = StandardScaler()
+X_ft = sc.fit_transform(X_feat.values)
+X_ft = pd.DataFrame(columns=X_feat.columns,
+                    data=X_ft,
+                    index=X_feat.index)
+
 
 plt.figure(figsize=(15,10))
 plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))
@@ -17,6 +50,4 @@ plt.xlabel('Scaled USD')
 plt.legend()
 plt.gcf().autofmt_xdate()
 plt.show()
-
-
 

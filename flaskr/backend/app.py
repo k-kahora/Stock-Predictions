904d04c08f86f87b577e00e6d607d57fbfbb1bf5
Malcolm Kahora <mango22mjk@gmail.com>
2023-04-10 18:16:41 -0400

    Seperated backend and frontend

---
+++ flaskr/backend/app.py
@@ -0,0 +1,24 @@
+from flask import Flask, request, jsonify
+import stockpredict 
+from flask_cors import CORS
+
+app = Flask(__name__)
+
+incomes = [
+    { 'description': 'salary', 'amount': 5000 }
+]
+
+@app.route('/predict/<symbol>')
+def get_stock(symbol):
+    return jsonify(stockpredict.plot_stock(symbol))
+
+@app.route('/incomes')
+def get_incomes():
+    return jsonify(incomes)
+
+
+@app.route('/incomes', methods=['POST'])
+def add_income():
+    incomes.append(request.get_json())
+    return '', 204
+CORS(app)
